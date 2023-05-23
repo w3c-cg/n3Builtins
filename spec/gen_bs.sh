e96187d6961db02a039ac57b6568a0102b16d141
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2023-05-23 19:01:01 +0200

    Fixing generation of index.bs

--- spec/gen_bs.sh
+++ spec/gen_bs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if command -v python3 &>/dev/null; then
-    python3 create-markdown.py > index.bs
+    python3 create-markdown.py 
 else
-    python create-markdown.py > index.bs
+    python create-markdown.py 
 fi
